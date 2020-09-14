0432d7a3040b787ff198c8e2f55b15505609eb2d
Maarten <maartenbeek15@gmail.com>
2020-09-14 14:31:41 +0200

    menu micro fixed

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,13 @@ var menuButton =
     document.querySelector('#hamburger');
 var nav = document.getElementById('navigation');
 
-menuButton.addEventListener('click', function () {
+//menuButton.addEventListener('click', function () {
+//    nav.classList.toggle('show-menu');
+//});
+
+menuButton.addEventListener('click', toonMenu)
+    
+function toonMenu() {
     nav.classList.toggle('show-menu');
+}
 
-
-
-});
