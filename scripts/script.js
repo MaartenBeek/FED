a0eb7cbf06d248218a81c9da4acbeeb6c7d2d8ca
Maarten <maartenbeek15@gmail.com>
2020-09-20 11:47:58 +0200

    menu update en fix

--- scripts/script.js
+++ scripts/script.js
@@ -3,13 +3,22 @@ var menuButton =
     document.querySelector('#hamburger');
 var nav = document.getElementById('navigation');
 
-//menuButton.addEventListener('click', function () {
-//    nav.classList.toggle('show-menu');
-//});
-
 menuButton.addEventListener('click', toonMenu)
     
 function toonMenu() {
     nav.classList.toggle('show-menu');
+}
+
+var zoekButton =
+    document.getElementById('zoek');
+var zoekBalk = document.getElementById('zoekbalk');
+var iconImage = document.getElementById('icon');
+
+zoekButton.addEventListener('click', toonBalk)
+    
+function toonBalk() {
+    zoekBalk.classList.toggle('show-balk');
+
+//    iconImage.src='close-icon.png
 }
 
