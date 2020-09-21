460815d090cb48a52a6bb6b770fa9c3d66513581
Maarten <maartenbeek15@gmail.com>
2020-09-21 11:30:19 +0200

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 var menuButton = document.querySelector('#hamburger');
-var nav = document.getElementById('#navigation');
+var nav = document.getElementById('navigation');
 
 menuButton.addEventListener('click', toonMenu)
     
@@ -8,16 +8,12 @@ function toonMenu() {
     nav.classList.toggle('show-menu');
 }
 
-var zoekButton =
-    document.getElementById('zoek');
+var zoekButton = document.getElementById('zoek');
 var zoekBalk = document.getElementById('zoekbalk');
-var iconImage = document.getElementById('icon');
 
 zoekButton.addEventListener('click', toonBalk)
     
 function toonBalk() {
     zoekBalk.classList.toggle('show-balk');
-
-//    iconImage.src='close-icon.png
 }
 
